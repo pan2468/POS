82ef1a687f10c02ffcafa284c8c87b3ed755a61e
임채성 <pan1129@naver.com>
2022-05-26 17:46:35 +0900

    Hello World Commit

---
+++ src/main/java/com/cafe/pos/HelloController.java
@@ -0,0 +1,16 @@
+package com.cafe.pos;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping
+    public String hello(){
+
+        return "hello";
+    }
+
+}
