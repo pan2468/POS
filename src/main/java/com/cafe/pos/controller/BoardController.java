bc17b143d7e77ae42964f8fbf3b8b9be2979dc4a
임채성 <pan1129@naver.com>
2022-05-27 15:36:18 +0900

    게시판 list 화면 Commit

---
+++ src/main/java/com/cafe/pos/controller/BoardController.java
@@ -0,0 +1,16 @@
+package com.cafe.pos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/list")
+    public String list(){
+
+        return "list";
+    }
+
+}
